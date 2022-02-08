f7f32b2f57bfd8da2cc235d62db3c968c3bc533e
carlos.ueda <carlos.ueda@gmail.com>
2022-02-08 05:08:01 -0400

    RXJava example

--- src/main/java/threads/RxJavaSimple.java
+++ src/main/java/threads/RxJavaSimple.java
@@ -1,4 +1,23 @@
 package threads;
 
+import rx.Observable;
+import rx.subjects.PublishSubject;
+
 public class RxJavaSimple {
+    public static void main(String[] args) {
+
+        Observable<String> observable = Observable.create(observer -> {
+            observer.onNext("One");
+            observer.onNext("Tow");
+        });
+        observable.map(s -> String.join(", ", s)).toSingle().subscribe(System.out::println);
+
+        PublishSubject<String> publishSubject = PublishSubject.create();
+        publishSubject.subscribe(s -> System.out.println("s=" + s));
+        publishSubject.subscribe(s -> System.out.println("d=" + s));
+        publishSubject.onNext("Three");
+        publishSubject.onNext("Four");
+        publishSubject.onNext("Five");
+        publishSubject.onCompleted();
+    }
 }
