943a826aed87401dd94bd3accd1f446bebc5c10f
carlos.ueda <carlos.ueda@gmail.com>
2022-02-09 10:48:02 -0400

    map in application.properties

--- src/main/java/com/example/gft/config/Config.java
+++ src/main/java/com/example/gft/config/Config.java
@@ -1,5 +1,7 @@
 package com.example.gft.config;
 
+import java.util.Map;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
@@ -9,8 +11,9 @@ import lombok.Data;
 @Component
 @Data
 @ImportResource
-@ConfigurationProperties(prefix = "spark")
+@ConfigurationProperties
 public class Config {
     private String appName;
     private String cpus;
+    public Map<String, String> externalMapping;
 }
--- src/test/java/com/example/gft/YmlTest.java
+++ src/test/java/com/example/gft/YmlTest.java
@@ -7,19 +7,26 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.YamlMapFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import com.example.gft.config.Config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
 import io.vavr.API;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest
 public class YmlTest {
     @Autowired
     public ResourceLoader resourceLoader;
+    @Autowired
+    public Config config;
 
     @Test
     public void ymlTest() {
@@ -30,5 +37,13 @@ public class YmlTest {
             return new ObjectMapper(new YAMLFactory()).readValue(file, Map.class);
         }).get();
         assertEquals("SELECT SYSDATE FROM DUAL", source.get("QUERY1"));
+    }
+
+    @Test
+    public void testExternalMappingResource() {
+        String EXPECTED = "c:/temp";
+        String actual = config.getExternalMapping().get("local");
+        log.info("Actual value {}", actual);
+        assertEquals(EXPECTED, actual);
     }
 }
