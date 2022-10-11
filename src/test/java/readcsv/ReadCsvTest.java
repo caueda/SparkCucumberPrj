be7859c633360d95e5d667113674f9c8bf79b8a4
Carlos Roberto Ueda <carlosrobertoueda@Carloss-MacBook-Pro.local>
2022-10-11 08:06:55 -0300

    CSV read complete

--- src/test/java/readcsv/ReadCsvTest.java
+++ src/test/java/readcsv/ReadCsvTest.java
@@ -3,26 +3,50 @@ package readcsv;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.util.FileCopyUtils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ExtendWith(SpringExtension.class)
 @Slf4j
 public class ReadCsvTest {
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @Test
     public void testReadCsv() throws Exception {
-        Resource resource = new ClassPathResource("classpath:rowproducer/rowProducer.csv");
-        InputStream inputStream = resource.getInputStream();
+        var resource = resourceLoader
+                .getResource("classpath:rowproducer/rowProducer.csv");
         try {
-            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
-            String data = new String(bdata, StandardCharsets.UTF_8);
-            log.info(data);
+            BufferedReader reader = new BufferedReader(new FileReader(resource.getFile()));
+            String line;
+            String header = null;
+            List<String> sql = new ArrayList<>();
+            while((line = reader.readLine()) != null) {
+                String[] values = line.split(";");
+                StringBuilder select = new StringBuilder();
+                if(header == null) {
+                    for(int i=0; i<values.length; i++) {
+                        select.append(" '%s' as " + values[i] + ((i<i-1) ? ", " : " "));
+                    }
+                    header = "select" + select.toString();
+                } else {
+                    sql.add(String.format(header, values));
+                }
+            }
+            System.out.println(sql.stream().collect(Collectors.joining(" union all ")));
         } catch (IOException e) {
             log.error("IOException", e);
         }
